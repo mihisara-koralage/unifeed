21f37a67d8dbf3d8d6d7b45969dd77b3520600dc
mihisara-koralage <mihisarakoralage023@gmail.com>
2026-07-07 15:49:41 +0530

    change the urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.views.static import serve
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/',       admin.site.urls),
-    path('users/',       include('users.urls')),
-    path('',             include('feed.urls')),
-    path('messages/',    include('messaging.urls')),
-    path('resources/',   include('resources.urls')),
-    path('events/',      include('events.urls')),
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('', include('feed.urls')),
+    path('messages/', include('messaging.urls')),
+    path('resources/', include('resources.urls')),
+    path('events/', include('events.urls')),
     path('marketplace/', include('marketplace.urls')),
-    path('dashboard/',   include('dashboard.urls')),
-    re_path(r'^static/(?P<path>.*)$', serve, {
-        'document_root': settings.STATIC_ROOT,
-        'show_indexes': False,
-    }),
-    re_path(r'^media/(?P<path>.*)$', serve, {
-        'document_root': settings.MEDIA_ROOT,
-        'show_indexes': False,
-    }),
+    path('dashboard/', include('dashboard.urls')),
 ]
